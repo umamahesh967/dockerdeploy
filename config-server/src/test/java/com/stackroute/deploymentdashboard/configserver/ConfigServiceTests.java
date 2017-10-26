5f892b07bf3f5606e4b5b233eb2bba71d1129ce2
sankhoroy <sankhoroy@gmail.com>
2017-10-26 19:32:01 +0530

    updated test cases for config server and discovery registry

--- config-server/src/test/java/com/stackroute/deploymentdashboard/configserver/ConfigServiceTests.java
+++ config-server/src/test/java/com/stackroute/deploymentdashboard/configserver/ConfigServiceTests.java
@@ -1,18 +1,22 @@
 
 package com.stackroute.deploymentdashboard.configserver;
 
+import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ConfigServiceTests {
 	
 	@Autowired
@@ -22,5 +26,20 @@ public class ConfigServiceTests {
 	public void contextLoads() {
 		assertFalse(this.environment.getPropertySources().contains("application"));
 	}
+	
+	@LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldStartEurekaServer() {
+    	
+        ResponseEntity<String> entity = this.testRestTemplate.getForEntity(
+                "http://localhost:" + this.port +"/apigateway/default" , String.class);
+
+        then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
 
