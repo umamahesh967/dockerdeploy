cbdef65f69bd38148d4bf19d040132da9920d6e2
shekharrajak <shekharrajak@live.com>
2017-10-27 16:54:54 +0530

    moved logics into service package

--- WorkflowEngineService/src/main/java/com/workflow/engine/exception/UrlException.java
+++ WorkflowEngineService/src/main/java/com/workflow/engine/exception/UrlException.java
@@ -1,11 +1,11 @@
 package com.workflow.engine.exception;
 
-public class UrlNotRepositoryException extends Exception {
+public class UrlException extends Exception {
 	static final long serialVersionUID = 43L;
 	
 	private String errormessage;
 	
-	public UrlNotRepositoryException(String errormessage) {
+	public UrlException(String errormessage) {
 		super(errormessage);
 		this.errormessage = errormessage;
 	}
