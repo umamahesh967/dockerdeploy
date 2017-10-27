4b6bcdab200832fcbfb7c939c88a1f060c2e584b
shekharrajak <shekharrajak@live.com>
2017-10-28 00:43:34 +0530

    kafka error : commented producer

--- WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/configuration/SwaggerConfig.java
+++ WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/configuration/SwaggerConfig.java
@@ -24,7 +24,6 @@ public class SwaggerConfig {
                 .build();
     }
     
-    @Bean
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
                 "Workflow Engine Service",
--- WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/messenger/ReportingServiceProducer.java
+++ WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/messenger/ReportingServiceProducer.java
@@ -5,18 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
  * For producer
- * */
-@Service
+ * 
+//@Service
 public class ReportingServiceProducer {
     
+	 private static final Logger LOGGER = LoggerFactory.getLogger(ReportingServiceProducer.class);
+
 	/*
 	 * for now testing with string. It will be replaced to ModelForJenkins.
 	 * 
-	 * */
+	 * 
 	@Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
     
@@ -26,9 +29,12 @@ public class ReportingServiceProducer {
     
     // refer : https://kafka.apache.org/quickstart
     public void send(String model) {
+    	LOGGER.info("sending payload='{}'", model);
         System.out.println("sending data=" + model);
         System.out.println(kafkaTopic);
         kafkaTemplate.send(kafkaTopic, model);
 //        System.out.println("sending done ...........yooooooooooooo" );
     }
 }
+
+*/
