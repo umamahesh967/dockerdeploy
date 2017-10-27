bb76d33314294ff0ed1cd0047ff47674b16360f5
shekharrajak <shekharrajak@live.com>
2017-10-27 17:06:02 +0530

    minor change

--- WorkflowEngineService/src/main/java/com/workflow/engine/repository/WorkflowCRUDRepository.java
+++ WorkflowEngineService/src/main/java/com/workflow/engine/repository/WorkflowCRUDRepository.java
@@ -1,7 +1,7 @@
 package com.workflow.engine.repository;
 
 import com.workflow.engine.model.ModelForJenkins;
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
 
 //public interface WorkflowCRUDRepository extends CrudRepository<ModelForJenkins, String> {
 //
