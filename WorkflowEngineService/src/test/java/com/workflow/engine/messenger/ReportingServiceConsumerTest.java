1204d38ddb6cae27cb7d4c53caf5d089792839f5
shekharrajak <shekharrajak@live.com>
2017-10-27 12:23:56 +0530

    test files added

---
+++ WorkflowEngineService/src/test/java/com/workflow/engine/messenger/ReportingServiceConsumerTest.java
@@ -0,0 +1,14 @@
+package com.workflow.engine.messenger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ReportingServiceConsumerTest {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
