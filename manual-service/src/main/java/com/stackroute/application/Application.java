07e45ccd24eacdad9d7f6e168098d4047c2b7b93
vamsi krishna <vamsi.spidervamsi@gmail.com>
2017-10-30 14:37:22 +0530

    trying to change the folder name

--- manual-service/src/main/java/com/stackroute/application/Application.java
+++ manual-service/src/main/java/com/stackroute/application/Application.java
@@ -6,12 +6,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class Application {	
-	public static void main(String[] args)  {
-				
-			SpringApplication.run(Application.class, args);
+@EnableDiscoveryClient
+public class Application {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(Application.class, args);
 	}
 }
-
