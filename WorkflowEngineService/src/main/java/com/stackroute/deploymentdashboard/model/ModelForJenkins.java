13f9d8018d6bf06f339850a62b03ad2e572bc132
shekharrajak <shekharrajak@live.com>
2017-10-30 15:09:28 +0530

    serialization issue fixed

--- WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/model/ModelForJenkins.java
+++ WorkflowEngineService/src/main/java/com/stackroute/deploymentdashboard/model/ModelForJenkins.java
@@ -1,14 +1,23 @@
 package com.stackroute.deploymentdashboard.model;
 
-public class ModelForJenkins {
+import java.io.Serializable;
+
+import org.springframework.context.annotation.Bean;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+public class ModelForJenkins implements Serializable {
 
 	private int projectID;
-
+	private String path;
 	/**
 	 * @param projectID
 	 */
-	public ModelForJenkins(int projectID) {
+
+	public ModelForJenkins(int projectID, String p) {
 		this.projectID = projectID;
+		this.path = p;
 	}
 	
 	
