e4e43af30cb2ca8a1f6a42a2f75b78c0f5e1088b
devendra.kumar <devendra.kumar@cgi.com>
2017-11-02 14:29:36 +0530

    integrated with parent pom

--- UserManagement/src/main/java/com/stackroute/deploymentdashboard/usermanagement/Application.java
+++ UserManagement/src/main/java/com/stackroute/deploymentdashboard/usermanagement/Application.java
@@ -1,12 +1,13 @@
 package com.stackroute.deploymentdashboard.usermanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
 @SpringBootApplication
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
