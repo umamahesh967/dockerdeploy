c176cb0c9111629f6485c80f62648dc50b3c53ff
sankhoroy <sankhoroy@gmail.com>
2017-11-03 04:14:32 +0530

    user repository and project repository updated for Neo4j Graph Operations

--- Ne04j-service/src/main/java/com/stackroute/deploymentdashboard/repository/ProjectRepository.java
+++ Ne04j-service/src/main/java/com/stackroute/deploymentdashboard/repository/ProjectRepository.java
@@ -1,5 +1,6 @@
 package com.stackroute.deploymentdashboard.repository;
 
+import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +8,6 @@ import com.stackroute.deploymentdashboard.model.ProjectModel;
 
 
 @Repository
-public interface ProjectRepository extends PagingAndSortingRepository<ProjectModel, Long>{
+public interface ProjectRepository extends Neo4jRepository<ProjectModel, Long>{
 
 }
